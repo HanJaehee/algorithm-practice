8e5739b990acaafd0406bcf69da71fca0139f488
HanJaehee <hindsight@kakao.com>
2023-12-21 21:54:57 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1560 ms, Memory: 163080 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -20,7 +19,8 @@ public class Main {
     }
 
 
-    Collections.sort(q);
+    q.sort((c1, c2) -> c1 - c2);
+//    Collections.sort(q);
     StringBuilder sb = new StringBuilder();
 
     q.forEach(s -> sb.append(s).append("\n"));
