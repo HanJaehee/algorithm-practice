b41c6a4a807d3d3b12edc664eed3aacde60ea54e
HanJaehee <hindsight@kakao.com>
2023-12-23 14:28:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1692 ms, Memory: 338996 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,41 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String[] nums = br.readLine().split(" ");
-    int N = Integer.parseInt(nums[0]);
-
-    List<Integer> list = new ArrayList<>();
-
+    int N = Integer.parseInt(br.readLine());
+    
     int[] numCnt = new int[10001];
+    
     for (int i = 0; i < N; i++) {
       int num = Integer.parseInt(br.readLine());
-      if (numCnt[num] == 0) {
-        list.add(num);
-      }
       numCnt[num]++;
     }
 
-    Collections.sort(list);
-
     StringBuilder sb = new StringBuilder();
 
-    list.forEach(a -> {
-      if (numCnt[a] == 1) {
-        sb.append(a).append("\n");
-      } else {
-        for (int i = 0; i < numCnt[a]; i++) {
-          sb.append(a).append("\n");
+    for (int i = 1; i <= 10000; i++) {
+      if(numCnt[i] != 0) {
+        for (int j = 0; j < numCnt[i]; j++) {
+          sb.append(i).append("\n");
         }
       }
-    });
+    };
 
     System.out.println(sb);
   }
